cddae78c4b32176a69ef9e78b0b3974be52a3c14
Victor Fontenele <victorvitoria11@hotmail.com>
2020-03-24 17:37:47 -0300

    chore: updated routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,20 @@
 import { Router } from 'express';
 
+import OngController from './app/controllers/OngController';
+import IncidentController from './app/controllers/IncidentController';
+import SessionController from './app/controllers/SessionController';
+
+
 const routes = new Router();
+
+routes.post('/session', SessionController.store);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ong/create', OngController.store);
+
+routes.get('/incidents', IncidentController.index);
+routes.get('/ong/incidents', IncidentController.show);
+routes.post('/incident/create', IncidentController.store);
+routes.delete('/incident/:id/delete', IncidentController.delete);
 
 export default routes;
